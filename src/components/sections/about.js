bb380dd604be2af840ce2e97377d475931a80d46
OsujiPius <piusosuji9@gmail.com>
2022-10-30 01:11:45 +0200

    Start of project

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -61,7 +61,6 @@ const StyledPic = styled.div`
     position: relative;
     width: 100%;
     border-radius: var(--border-radius);
-    background-color: var(--green);
 
     &:hover,
     &:focus {
@@ -81,8 +80,6 @@ const StyledPic = styled.div`
     .img {
       position: relative;
       border-radius: var(--border-radius);
-      mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1);
       transition: var(--transition);
     }
 
@@ -125,7 +122,16 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = [
+    'JavaScript (ES6+)',
+    'TypeScript',
+    'React',
+    'Next',
+    'Hasura',
+    'GraphQl',
+    'Styled-components',
+    'Tailwind css',
+  ];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -135,30 +141,20 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Pius. I have always had an interest with working with computers and
+              understanding how they work. This made me pursue a career in computer science.
             </p>
-
+            <p>
+              I have improved significantly since starting out, from making static HTML/CSS sites to
+              more complex projects. i have been praised as resourceful and dilligent by my peers
+              and teachers.
+            </p>
             <p>
               Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
-            </p>
-
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              <a href="https://upwork.com/">freelancing website</a>,{' '}
+              <a href="https://credib.io/">a start-up</a>, and . My main focus these days is
+              building accessible, inclusive products experiences at{' '}
+              <a href="https://offscript.io/">Offscript</a> for a variety of clients.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
@@ -173,7 +169,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.jpg"
+              src="../../images/pius.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,28 +59,24 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
+  const two = <h2 className="big-heading">Pius Osuji.</h2>;
   const three = <h3 className="big-heading">I build things for the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
+        I’m a software engineer specializing in building different software solutions primarily for
+        the web. Currently, I'm focused on building a unique product that will allow creators have
+        their own personalized shop at {''}
+        <a href="https://offscript.io/" target="_blank" rel="noreferrer">
+          Offscript
         </a>
         .
       </p>
     </>
   );
   const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
+    <a className="email-link" href="mailto:osujipius7@gmail.com" target="_blank" rel="noreferrer">
+      Reach out!
     </a>
   );
 
